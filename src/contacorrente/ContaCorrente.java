e510fdb2d5c0dad655c6c67dc1cc08ac3aa85189
marciadaniel <marciadaniel202@gmail.com>
2022-10-04 21:16:07 +0100

    Alterações em ambas as classes

--- src/contacorrente/ContaCorrente.java
+++ src/contacorrente/ContaCorrente.java
@@ -3,56 +3,62 @@ package contacorrente;
 import java.util.Scanner;
 
 public class ContaCorrente {
-	
+
 	int numeroDaConta;
 	double saldoDaConta;
-	boolean contaEspecial;
-	double valorLimiteDaConta;
-	
-	void realizarSaque() {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Insira o valor a levantar:");
-		double valorALevantar = scan.nextDouble();
-		
-		if(valorALevantar <= saldoDaConta) {
+	boolean contaEspecial = true;
+	double limiteChequeEspecial;
+	double limiteDaConta;
+
+	boolean realizarSaque(double valorALevantar) {
+
+		if (valorALevantar <= saldoDaConta) {
 			saldoDaConta = saldoDaConta - valorALevantar;
-		}else {
-		System.out.println("Não há dinheiro suficiente na conta para poder efectuar este saque!");
-	}
-	  }
-	
-	void RealizarDeposito() {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Insira o valor a depositar:");
-		double valorADepositar = scan.nextDouble();
-		
-		double saldoMaisDeposito = saldoDaConta + valorADepositar;
-		
-		if(saldoMaisDeposito <= valorLimiteDaConta ) {
-			saldoDaConta = saldoDaConta - valorADepositar;
-		}else {
-		System.out.println("Valor limite  da conta excedido!\nValor limite da conta:" + valorLimiteDaConta);
-	}
-		
-	}
-	
-	void consultarSaldo() {
-		
-		System.out.println("O saldo disponível é:" + saldoDaConta);
-				
-	}
-	
-	void verificarChequeEspecial() {
-		
-		if(saldoDaConta < 0) {
-		System.out.println("Está usando o cheque especial!");
+			return true;
+
+		} else if (saldoDaConta > 0 && saldoDaConta < valorALevantar
+				&& limiteChequeEspecial >= (valorALevantar - saldoDaConta)) {
+			valorALevantar -= saldoDaConta;
+			saldoDaConta -= saldoDaConta;
+			saldoDaConta = saldoDaConta - valorALevantar;
+			limiteChequeEspecial -= valorALevantar;
+			return true;
+		} else if (contaEspecial && valorALevantar <= limiteChequeEspecial) {
+			saldoDaConta = saldoDaConta - valorALevantar;
+			limiteChequeEspecial -= valorALevantar;
+			return true;
+		} else {
+			return false;
 		}
-		
 	}
-		
-		
+
+	void RealizarDeposito(double valorADepositar) {
+
+		Scanner scan = new Scanner(System.in);
+
+		double saldoMaisDeposito = saldoDaConta + valorADepositar;
+
+		if (saldoMaisDeposito <= limiteDaConta) {
+			saldoDaConta = saldoDaConta - valorADepositar;
+		} else {
+			System.out.println("Valor limite  da conta excedido!\nValor limite da conta:" + limiteDaConta);
+		}
+
+	}
+
+	void consultarSaldo() {
+
+		System.out.println("O saldo disponível é:" + saldoDaConta);
+
+	}
+
+	boolean verificarChequeEspecial() {
+
+		if (saldoDaConta < 0) {
+			return true;
+		} else
+			return false;
+
+	}
+
 }
--- src/contacorrente/TesteContaCorrente.java
+++ src/contacorrente/TesteContaCorrente.java
@@ -11,40 +11,60 @@ public class TesteContaCorrente {
 		short opcao;
 
 		ContaCorrente conta = new ContaCorrente();
-		
+
 		conta.contaEspecial = true;
 		conta.numeroDaConta = 001;
-		conta.saldoDaConta = 200.00;
-		conta.valorLimiteDaConta = 200000;
-		
-		
-		System.out.println("Menu");
-		System.out.println("Digite 1 para Saque");
-		System.out.println("Digite 2 para Consultar Saldo");
-		System.out.println("Digite 3 para Depositar");
-		System.out.println("Digite 4 para verificar se está usando cheque especial");
-		
-		opcao = scan.nextShort();
+		conta.saldoDaConta = 200;
+		conta.limiteChequeEspecial = 200;
 
-		switch (opcao) {
-		case 1:
-			conta.realizarSaque();
-			break;
-		case 2:
-			conta.consultarSaldo();
-			break;
-		case 3:
-			conta.RealizarDeposito();
-			break;
-		case 4:
-			conta.verificarChequeEspecial();
-			break;
-		default:
-			System.out.println("Opção inválida!");
-		}
-		
-		
+		do {
 
+			System.out.println("Menu");
+			System.out.println("Digite 1 para Saque");
+			System.out.println("Digite 2 para Depositar");
+			System.out.println("Digite 3 para Consultar Saldo ");
+			System.out.println("Digite 4 para verificar se está usando cheque especial");
+			System.out.println("Digite 0 para sair");
+
+			opcao = scan.nextShort();
+
+			switch (opcao) {
+			case 1:
+				System.out.print("Quanto deseja levantar?\nR:");
+				double valorALevantar = scan.nextDouble();
+
+				boolean levantamentoEfectuado = conta.realizarSaque(valorALevantar);
+
+				if (levantamentoEfectuado) {
+					System.out.println("Levantamento efectuado com sucesso!\n Valor levantado:" + valorALevantar
+							+ "Saldo actual:" + conta.saldoDaConta);
+				} else {
+					System.out.println("Não foi possível efectuar o levantamento, o saldo é insuficiente!");
+				}
+				break;
+			case 2:
+				System.out.print("Insira o valor a depositar:");
+				double valorADepositar = scan.nextDouble();
+				conta.RealizarDeposito(valorADepositar);
+
+				break;
+			case 3:
+				conta.consultarSaldo();
+
+				break;
+			case 4:
+				boolean verificacao = conta.verificarChequeEspecial();
+				if (verificacao) {
+					System.out.println("Está usando o cheque especial!");
+				} else {
+					System.out.println("Não está usando o cheque especial!");
+				}
+				break;
+			default:
+				System.out.println("Opção inválida!");
+			}
+
+		} while (opcao != 0);
 	}
 
 }
